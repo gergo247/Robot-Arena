c997f5126d0d1a300765f99849745b88488d6d26
gergo247 <gergo247@gmail.com>
2020-07-09 22:26:17 +0200

    Animation fix, Spawn/wave system beggining

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public float spawnRadius = 10f;
+
+    [HideInInspector]
+    public Wave currentWave;
+
+    private float nextSpawnTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnWave();
+            nextSpawnTime = Time.time + 1f / currentWave.spawnRate;
+        }
+    }
+
+    void SpawnWave()
+    {
+        foreach (GameObject eType in currentWave.enemies)
+        {
+            //if (Random.value <= eType.spawnChance)
+            //{
+                SpawnEnemy(eType);
+           // }
+        }
+    }
+
+    void SpawnEnemy(GameObject enemyPrefab)
+    {
+        Vector2 spawnPos = Vector3.zero;
+        spawnPos += Random.insideUnitCircle.normalized * spawnRadius;
+
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+
+}
