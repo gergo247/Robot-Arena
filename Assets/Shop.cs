a250d8b0c76dca3965187f19e0949f8de5e9d844
gergo247 <gergo247@gmail.com>
2020-07-12 16:31:39 +0200

    Wave + shop v2

---
+++ Assets/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public static Shop instance;
+    public List<GameObject> Characters;
+
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogError("More than one shop in scene.");
+        else
+            instance = this;
+    }
+
+
+    void Start()
+    {
+       // PlayerTeam.instance.PutIntoTeam(Characters[0]);
+    }
+
+}
